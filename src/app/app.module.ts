639460c75e7e2025e48d7d7d9fd430971ba23c78
piki1489 <piki1489@hotmail.com>
2023-03-02 22:06:15 +0100

    componentes agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PresupuestoComponent } from './components/presupuesto/presupuesto.component';
+import { ComprasComponent } from './components/compras/compras.component';
+import { VentasComponent } from './components/ventas/ventas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    PresupuestoComponent,
+    ComprasComponent,
+    VentasComponent
   ],
   imports: [
     BrowserModule,
